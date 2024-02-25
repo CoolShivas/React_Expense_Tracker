e0acf01e4e85820022454c75d75eb2018aba11be
CoolShivas <shivas2710cool00@gmail.com>
2024-02-26 01:27:51 +0530

    Passing_Data_Via_Props

--- expense_tracker_app/src/App.jsx
+++ expense_tracker_app/src/App.jsx
@@ -2,9 +2,57 @@ import AppName from "./components/AppName";
 import ExpenseItems from "./components/ExpenseItems";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "auto insurance",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "bike insurance",
+      amount: 194.12,
+      date: new Date(2021, 1, 28),
+    },
+    {
+      id: "e3",
+      title: "car insurance",
+      amount: 2594.12,
+      date: new Date(2022, 5, 1),
+    },
+    {
+      id: "e4",
+      title: "dumper insurance",
+      amount: 1294.12,
+      date: new Date(2023, 9, 21),
+    },
+  ];
   return <>
     <AppName></AppName>
-    <ExpenseItems></ExpenseItems>
+    <ExpenseItems
+      titleABC={expenses[0].title}
+      amountABC={expenses[0].amount}
+      dateABC={expenses[0].date}
+    ></ExpenseItems>
+
+    <ExpenseItems
+      titleABC={expenses[1].title}
+      amountABC={expenses[1].amount}
+      dateABC={expenses[1].date}
+    ></ExpenseItems>
+
+    <ExpenseItems
+      titleABC={expenses[2].title}
+      amountABC={expenses[2].amount}
+      dateABC={expenses[2].date}
+    ></ExpenseItems>
+
+    <ExpenseItems
+      titleABC={expenses[3].title}
+      amountABC={expenses[3].amount}
+      dateABC={expenses[3].date}
+    ></ExpenseItems>
   </>
 }
 export default App;
--- expense_tracker_app/src/components/ExpenseItems.jsx
+++ expense_tracker_app/src/components/ExpenseItems.jsx
@@ -1,13 +1,13 @@
-const ExpenseItems = () => {
+const ExpenseItems = (props) => {
 
-    const date = new Date(2021, 2, 12);
-    const title = "auto insurance";
-    const amount = 2561;
+    // const date = new Date(2021, 2, 12);
+    // const title = "auto insurance";
+    // const amount = 2561;
 
     return <>
-        <div> {date.toISOString()} </div>
-        <div> {title} </div>
-        <div> {amount} </div>
+        <div> {props.dateABC.toISOString()} </div>
+        <div> {props.titleABC} </div>
+        <div> {props.amountABC} </div>
     </>
 }
 
